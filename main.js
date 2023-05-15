2bf4b9750259f85f74c335838e23cecd89a6ca9d
edu <133611193+eduflores22@users.noreply.github.com>
2023-05-16 02:36:05 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+document.getElementById("contexto-btn").addEventListener("click", function() {
+    var container = document.getElementById("contexto-container");
+    if (container.style.display === "none") {
+        container.style.display = "block";
+    } else {
+        container.style.display = "none";
+    }
+});
+document.getElementById("Fuentes-btn").addEventListener("click", function() {
+    var container = document.getElementById("Fuentes-container");
+    if (container.style.display === "none") {
+        container.style.display = "block";
+    } else {
+        container.style.display = "none";
+    }
+});
+document.getElementById("PrincipiosHtml-btn").addEventListener("click", function() {
+    var container = document.getElementById("PrincipiosHtml-container");
+    if (container.style.display === "none") {
+        container.style.display = "block";
+    } else {
+        container.style.display = "none";
+    }
+});
+document.getElementById("PrincipiosCSS-btn").addEventListener("click", function() {
+    var container = document.getElementById("PrincipiosCSS-container");
+    if (container.style.display === "none") {
+        container.style.display = "block";
+    } else {
+        container.style.display = "none";
+    }
+});
+document.getElementById("PrincipiosJavaScript-btn").addEventListener("click", function() {
+    var container = document.getElementById("PrincipiosJavaScript-container");
+    if (container.style.display === "none") {
+        container.style.display = "block";
+    } else {
+        container.style.display = "none";
+    }
+});
+document.getElementById("vscode-btn").addEventListener("click", function() {
+    var container = document.getElementById("vscode-container");
+    if (container.style.display === "none") {
+        container.style.display = "block";
+    } else {
+        container.style.display = "none";
+    }
+});
